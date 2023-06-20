d078aa0ed6539125db43c301adf524d9b640044b
CesarJZO <cesarjzo@outlook.com>
2023-06-19 19:22:33 -0600

    Add Grid custom control and background style

---
+++ Assets/Editor/BehaviourTreeView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+
+namespace Editor
+{
+    public class BehaviourTreeView : GraphView
+    {
+        public new class UxmlFactory : UxmlFactory<BehaviourTreeView, UxmlTraits> { }
+
+        public BehaviourTreeView()
+        {
+            Insert(0, new GridBackground());
+            var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/BehaviourTreeEditor.uss");
+            styleSheets.Add(styleSheet);
+        }
+    }
+}
