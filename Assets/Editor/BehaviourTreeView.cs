cb28d648162f9b38b62fc5985bef0f6a0de6c466
CesarJZO <cesarjzo@outlook.com>
2023-06-19 19:36:05 -0600

    Add SplitView.cs and InspectorView.cs and a toolbar from UI Builder

--- Assets/Editor/BehaviourTreeView.cs
+++ Assets/Editor/BehaviourTreeView.cs
@@ -11,6 +11,12 @@ namespace Editor
         public BehaviourTreeView()
         {
             Insert(0, new GridBackground());
+
+            this.AddManipulator(new ContentZoomer());
+            this.AddManipulator(new ContentDragger());
+            this.AddManipulator(new SelectionDragger());
+            this.AddManipulator(new RectangleSelector());
+
             var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/BehaviourTreeEditor.uss");
             styleSheets.Add(styleSheet);
         }
