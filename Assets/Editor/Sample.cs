5e4c34e3b4df084b23e6c69fa47ca0e6a96bc371
CesarJZO <cesarjzo@outlook.com>
2023-06-19 19:05:51 -0600

    Add example using ObjectField

--- Assets/Editor/Sample.cs
+++ Assets/Editor/Sample.cs
@@ -1,8 +1,7 @@
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
-
 
 public class Sample : EditorWindow
 {
@@ -24,8 +23,8 @@ public class Sample : EditorWindow
 
         // Import UXML
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/Sample.uxml");
-        VisualElement labelFromUXML = visualTree.Instantiate();
-        root.Add(labelFromUXML);
+        VisualElement labelFromUxml = visualTree.Instantiate();
+        root.Add(labelFromUxml);
 
         // A stylesheet can be added to a VisualElement.
         // The style will be applied to the VisualElement and all of its children.
@@ -33,5 +32,9 @@ public class Sample : EditorWindow
         VisualElement labelWithStyle = new Label("Hello World! With Style");
         labelWithStyle.styleSheets.Add(styleSheet);
         root.Add(labelWithStyle);
+
+        // Add listener to button1
+        var button1 = root.Q<Button>("button1");
+        button1.RegisterCallback<MouseUpEvent>(e => Debug.Log("Button 1 pressed!"));
     }
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item", order = 0)]
+public class Item : ScriptableObject
+{
+    public string itemName;
+}
